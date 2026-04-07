79259e73c8efab2b6b542c6b95f5c3314796fec9
ChaeHyunLim <chaehyun010104@gmail.com>
2026-04-07 19:33:04 +0900

    setting : 초기 프로젝트 생성

---
+++ src/test/java/com/example/spring_mvc_study1_servlet/SpringMvcStudy1ServletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_mvc_study1_servlet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcStudy1ServletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
