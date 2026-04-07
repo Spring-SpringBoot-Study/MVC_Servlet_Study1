94db4b8f4fab17fd4530c384a4e9b1607ea15a8d
ChaeHyunLim <chaehyun010104@gmail.com>
2026-04-07 21:29:34 +0900

    section3 : HTTP 요청 데이터 - API 메시지 바디 - JSON

---
+++ src/main/java/com/example/spring_mvc_study1_servlet/basic/HelloData.java
@@ -0,0 +1,12 @@
+package com.example.spring_mvc_study1_servlet.basic;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class HelloData {
+
+    private String username;
+    private int age;
+}
