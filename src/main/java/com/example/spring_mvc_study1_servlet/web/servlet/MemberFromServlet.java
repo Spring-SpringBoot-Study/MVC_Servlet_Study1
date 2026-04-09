786e633d3cf4bd6e5191be46a9d22564be0643ff
ChaeHyunLim <chaehyun010104@gmail.com>
2026-04-09 13:02:25 +0900

    section4 : 서블릿으로 회원 관리 웹 애플리케이션 만들기

---
+++ src/main/java/com/example/spring_mvc_study1_servlet/web/servlet/MemberFromServlet.java
@@ -0,0 +1,40 @@
+package com.example.spring_mvc_study1_servlet.web.servlet;
+
+import com.example.spring_mvc_study1_servlet.domain.Member.MemberRepository;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "memberFromServlet", urlPatterns = "/servlet/members/new-form")
+public class MemberFromServlet extends HttpServlet {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        response.setCharacterEncoding("UTF-8");
+
+        // 서블릿으로 할 경우, 아래와 같이 자바 코드로 html을 작성해야하기 때문에 불편함
+        PrintWriter w = response.getWriter();
+        w.write("<!DOCTYPE html>\n" +
+                "<html>\n" +
+                "<head>\n" +
+                "    <meta charset=\"UTF-8\">\n" +
+                "    <title>Title</title>\n" +
+                "</head>\n" +
+                "<body>\n" +
+                "<form action=\"/servlet/members/save\" method=\"post\">\n" +
+                "    username: <input type=\"text\" name=\"username\" />\n" +
+                "    age:      <input type=\"text\" name=\"age\" />\n" +
+                "    <button type=\"submit\">전송</button>\n" +
+                "</form>\n" +
+                "</body>\n" +
+                "</html>\n");
+    }
+}
